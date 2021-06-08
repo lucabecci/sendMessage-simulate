1a94a99a4a92150da84c7cdb1956cf64d1baa969
lucabecci <beccibrian@gmail.com>
2021-06-08 20:15:09 -0300

    chore(): First practise added

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+  const [message, setMessage] = React.useState("")
+  const [submit, setsubmit] = React.useState("")
+
+  const final = (t) => {
+    alert(t)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Send message</Text>
+      <TextInput 
+      style={styles.input}
+      placeholder="Insert the message on here"
+      onChangeText={t => setMessage(t)}
+      value={message}
+    />
+    <Button 
+      title="Send"
+      onPress={() => {
+        setsubmit(message)
+        final(message)
+        setMessage("") 
+        setsubmit("") 
+      }}
+    />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    marginBottom:5,
+    marginTop: 5,
+    width: "100%"
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
